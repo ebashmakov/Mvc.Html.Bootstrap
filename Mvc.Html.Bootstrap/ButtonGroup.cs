8581fac96a888ddc680b4e706ff7d3d8d316a93a
crackbrain <mail@bashmakov.info>
2012-09-30 19:50:34 +0700

    Adding ButtonGroupExtensions

---
+++ Mvc.Html.Bootstrap/ButtonGroup.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Mvc.Html.Bootstrap
+{
+    public enum Direction
+    {
+        Horizontal, Vertical
+    }
+
+    public static class ButtonGroup
+    {
+        private static class Css
+        {
+            public static readonly string Group;
+            public static readonly string VerticalGroup;
+            public static readonly string Toolbar;
+
+            static Css()
+            {
+                Group = "btn-group";
+                VerticalGroup = string.Format("{0} {1}-vertical", Group, Group);
+                Toolbar = "btn-toolbar";
+            }
+        }
+
+        public static MvcForm BeginButtonGroup(this HtmlHelper html)
+        {
+            return BeginButtonGroup(html, Direction.Horizontal);
+        }
+
+        public static MvcForm BeginButtonGroup(this HtmlHelper html, Direction direction)
+        {
+            var cssClass = direction == Direction.Vertical
+                ? Css.VerticalGroup
+                : Css.Group;
+            return html.ContainerHelper(new { @class = cssClass });
+        }
+
+        public static MvcForm BeginButtonToolbar(this HtmlHelper html)
+        {
+            return html.ContainerHelper(new { @class = Css.Toolbar });
+        }
+    }
+}
---
+++ Mvc.Html.Bootstrap/ContainerExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Mvc.Html.Bootstrap
+{
+    public static class ContainerExtensions
+    {
+        internal static MvcForm ContainerHelper(this HtmlHelper html, object htmlAttributes)
+        {
+            return ContainerHelper(html, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+        }
+
+        internal static MvcForm ContainerHelper(this HtmlHelper html, IDictionary<string, object> htmlAttributes)
+        {
+            var tagBuilder = new TagBuilder("div");
+            tagBuilder.MergeAttributes(htmlAttributes);
+
+            html.ViewContext.Writer.Write(tagBuilder.ToString(TagRenderMode.StartTag));
+
+            return new MvcForm(html.ViewContext); ;
+        }
+
+        public static void EndContainer(this HtmlHelper html)
+        {
+            EndContainer(html.ViewContext);
+        }
+
+        internal static void EndContainer(ViewContext viewContext)
+        {
+            viewContext.Writer.Write("</div>");
+        }
+    }
+}
