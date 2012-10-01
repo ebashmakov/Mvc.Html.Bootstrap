4bb525541e3584f985135a243361b60993b33d3b
crackbrain <mail@bashmakov.info>
2012-10-02 01:13:37 +0700

    Added Tests project and enabled nuget

--- Mvc.Html.Bootstrap.Tests/Properties/AssemblyInfo.cs
+++ Mvc.Html.Bootstrap.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mvc.Html.Bootstrap")]
+[assembly: AssemblyTitle("Mvc.Html.Bootstrap.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mvc.Html.Bootstrap")]
-[assembly: AssemblyCopyright("Copyright © bashmakov.info 2012")]
+[assembly: AssemblyProduct("Mvc.Html.Bootstrap.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("00383223-8aca-48f1-ac49-b4db47096e49")]
+[assembly: Guid("e0bbba4e-6eab-4279-9a02-29be78557be4")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
