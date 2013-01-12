74cd3afcde855c70be339b2ee32af21b5da20acd
Aaron jackson <aaron.jackson@gmail.com>
2013-01-12 09:34:51 -0700

    Modified menu views to highlight current nodes.

--- Mvc.Html.Bootstrap/MenuExtensions.cs
+++ Mvc.Html.Bootstrap/MenuExtensions.cs
@@ -5,17 +5,33 @@ using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using MvcSiteMapProvider;
+using MvcSiteMapProvider.Web.Html.Models;
 
 namespace Mvc.Html.Bootstrap
 {
     public static class MenuExtensions
     {
-        public static IHtmlString GetBootstrappCssClass(this HtmlHelper html, MvcSiteMapProvider.Web.Html.Models.SiteMapNodeModel node) {
+        public static IHtmlString GetBootstrappCssClass(this HtmlHelper html, SiteMapNodeModel node) {
             var css = new List<string>();
-            if (node.IsCurrentNode) { css.Add("active"); }
+
+            if (node.IsCurrentNode || node.HasCurrentNode()) { css.Add("active"); }
             if (node.Children.Any()) { css.Add("dropdown"); }
 
-            return html.Raw(css.Any() ? "class=\"" + String.Join(" ", css) + "\"" : string.Empty);
+            return html.Raw(css.Any() ? String.Join(" ", css) : string.Empty);
+        }
+
+        public static bool HasCurrentNode(this SiteMapNodeModel node)
+        {
+            if (node.Children.Where(n => n.IsCurrentNode).Any())
+                return true;
+
+            foreach (var childNode in node.Children)
+            {
+                if (childNode.HasCurrentNode())
+                    return true;
+            }
+
+            return false;
         }
     }
 }
