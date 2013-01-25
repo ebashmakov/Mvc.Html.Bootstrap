4b94fbc0bd0f2facee91d158845cba66e6946ff2
Aaron jackson <aaron.jackson@gmail.com>
2013-01-25 09:49:50 -0700

    Started documentation

--- Mvc.Bootstrap.Web/Controllers/DocumentationController.cs
+++ Mvc.Bootstrap.Web/Controllers/DocumentationController.cs
@@ -6,16 +6,25 @@ using System.Web.Mvc;
 
 namespace Mvc.Bootstrap.Web.Controllers
 {
-    public class BootstrapController : Controller
+    public class DocumentationController : Controller
     {
         //
-        // GET: /Bootstrap/
+        // GET: /Documentation/
 
         public ActionResult Index()
         {
             return View();
         }
 
+        // 
+        // GET: /Forms/
+        public ActionResult Forms()
+        {
+            return View(new Mvc.Bootstrap.Web.Models.SampleModel());
+        }
+
+        //
+        // Handle any requests to /Documentation/* and show the view associated with the /* of the URL.
         protected override void HandleUnknownAction(string actionName)
         {
             this.View(actionName).ExecuteResult(this.ControllerContext);
---
+++ Mvc.Bootstrap.Web/Models/DocumentationModels.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mvc.Bootstrap.Web.Models
+{
+    public class SampleModel
+    {
+        [Required]
+        [Display(Name = "Sample")]
+        public string Sample { get; set; }
+    }
+}
